f697bb54e3885b5c8df8c09aa123d2fd1f08d34f
r2aiv <l0calhost@mail.ru>
2019-09-22 22:04:42 +0300

    MyForm initial

--- Flash/retarget.d
+++ Flash/retarget.d
@@ -1,4 +1,4 @@
 .\flash\retarget.o: Retarget.c
-.\flash\retarget.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_misc.h
-.\flash\retarget.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\flash\retarget.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\flash\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_misc.h
+.\flash\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\flash\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
